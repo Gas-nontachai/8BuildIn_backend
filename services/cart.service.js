54d8755bba23f4447d2c4269e4e276806bb5c5da
oanbibii1127 <nonthawat.kham@gmail.com>
2025-03-06 17:47:49 +0700

    fix getCartBy

--- services/cart.service.js
+++ services/cart.service.js
@@ -8,7 +8,7 @@ Task.getCartByID = (connection, data) => CartModel.getCartByID(connection, data)
 Task.getCartPermissionBy = (connection, data) => CartModel.getCartPermissionBy(connection, data)
 
 Task.insertCart = async (connection, data) => {
-    const res = await CartModel.insertCart(connection, {
+    const res = await CartModel.getCartBy(connection, {
         match: {
             $and: [
                 { product_id: data.product_id },
@@ -20,9 +20,11 @@ Task.insertCart = async (connection, data) => {
         return { message: "already in cart." };
     } else {
         data.cart_id = await CartModel.generateCartID(connection)
+        // console.log(data)
         await CartModel.insertCart(connection, data);
         return await CartModel.getCartByID(connection, { cart_id: data.cart_id });
     }
+   
 };
 
 Task.updateCartBy = async (connection, data) => {
