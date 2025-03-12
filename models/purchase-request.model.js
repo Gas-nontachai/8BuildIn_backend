7e59280781e42b52b1502b06d49c62660b8b571d
ppprakit <kit14122540@gmail.com>
2025-03-12 22:04:01 +0700

    edit API RP

--- models/purchase-request.model.js
+++ models/purchase-request.model.js
@@ -1,51 +1,98 @@
 const Task = function (task) {
-    this.task = task.task
-    this.created_at = new Date()
+  this.task = task.task
+  this.created_at = new Date()
 }
 
 const { formatDate } = require('@/utils/date-helper')
 const { generateQuery, mapToCondition } = require("@/utils/db-helper")
 
 Task.generatePurchaseRequestID = (connection) => new Promise((resolve, reject) => {
-    let code = `ST-IN${formatDate(new Date(), 'yyyyMMdd')}-`
-    let digit = 3
+  let code = `PR${formatDate(new Date(), 'yyyyMMdd')}-`
+  let digit = 3
 
-    let sql = `SELECT CONCAT(${connection.escape(code)}, LPAD(IFNULL(MAX(CAST(SUBSTRING(pr_id,${(code.length + 1)},${digit}) AS SIGNED)),0) + 1,${digit},0)) AS id 
-          FROM tb_purchase_request
-          WHERE pr_id LIKE (${connection.escape(`${code}%`)}) 
-      `
-    connection.query(sql, function (err, res) { err ? reject(new Error(err.message)) : resolve(res[0].id) })
+  let sql = `SELECT CONCAT(${connection.escape(code)}, LPAD(IFNULL(MAX(CAST(SUBSTRING(pr_id,${(code.length + 1)},${digit}) AS SIGNED)),0) + 1,${digit},0)) AS id 
+		FROM tb_purchase_request
+		WHERE pr_id LIKE (${connection.escape(`${code}%`)}) 
+	`
+  connection.query(sql, function (err, res) { err ? reject(new Error(err.message)) : resolve(res[0].id) })
+})
+
+Task.getPurchaseRequestBy = (connection, data = {}) => new Promise((resolve, reject) => {
+  let condition = mapToCondition(data)
+  const { filter, pagination, sort, group } = generateQuery(data)
+
+  const core_query = `SELECT *
+    FROM tb_purchase_request AS tb
+    WHERE TRUE
+    ${condition}
+    ${filter} 
+  `
+  const count_query = `SELECT COUNT(*) AS total FROM (${core_query}) AS tb`
+
+  if (data.count) return connection.query(count_query, function (err, res_total) {
+    err ? reject(new Error(err.message)) : resolve(res_total[0].total)
+  })
+
+  connection.query(`${core_query} ${group} ${sort} ${pagination}`, function (err, res) {
+    if (err) return reject(new Error(err.message))
+    if (!pagination) return resolve({ docs: res, totalDocs: res.length })
+
+    connection.query(count_query, function (err, res_total) {
+      err ? reject(new Error(err.message)) : resolve({ docs: res, totalDocs: res_total[0].total })
+    })
+  })
 })
 
 Task.getPurchaseRequestByID = (connection, data = {}) => new Promise((resolve, reject) => {
-    let sql = `SELECT * FROM tb_purchase_request
-      WHERE pr_id = ${connection.escape(data.pr_id)}
-      `
-    connection.query(sql, function (err, res) {
-        if (err) return reject(new Error(err.message))
-        if (!res.length) return data.required === false ? resolve() : reject(new Error('Not Found'))
+  let sql = `SELECT * FROM tb_purchase_request
+    WHERE pr_id = ${connection.escape(data.pr_id)}
+    `
+  connection.query(sql, function (err, res) {
+    if (err) return reject(new Error(err.message))
+    if (!res.length) return data.required === false ? resolve() : reject(new Error('Not Found'))
 
-        resolve(res[0])
-    })
+    resolve(res[0])
+  })
 })
 
 Task.insertPurchaseRequest = (connection, data = {}) => new Promise((resolve, reject) => {
-    let sql = `INSERT INTO tb_purchase_request (
-      pr_id,
-      pr_status,
-      pr_note,
-      product,
-      material,
-      addby,
-      adddate
-    ) VALUES (
-      ${connection.escape(data.pr_id)},
-      ${connection.escape(data.pr_status)}, 
-      ${connection.escape(data.pr_note)}, 
-      ${connection.escape(data.product)}, 
-      ${connection.escape(data.material)}, 
-      ${connection.escape(connection.session._id)},
-      NOW()
-    )`
-    connection.query(sql, function (err, res) { err ? reject(new Error(err.message)) : resolve(res) })
+  let sql = `INSERT INTO tb_purchase_request (
+    pr_id, 
+    pr_status,
+    pr_note,
+    product,
+    material,
+    addby,
+    adddate
+  ) VALUES (
+    ${connection.escape(data.pr_id)},
+    ${connection.escape(data.pr_status)}, 
+    ${connection.escape(data.pr_note)},
+    ${connection.escape(data.product)},
+    ${connection.escape(data.material)},
+    ${connection.escape(connection.session._id)},
+    NOW()
+  )`
+  connection.query(sql, function (err, res) { err ? reject(new Error(err.message)) : resolve(res) })
 })
+
+Task.updatePurchaseRequestBy = (connection, data = {}) => new Promise((resolve, reject) => {
+  let sql = `UPDATE tb_purchase_request SET    
+  pr_status = ${connection.escape(data.pr_status)}, ,
+  pr_note = ${connection.escape(data.pr_note)},
+  product = ${connection.escape(data.product)},
+  material = ${connection.escape(data.material)},
+  updateby = ${connection.escape(connection.session._id)},
+  lastupdate = NOW()
+  WHERE pr_id = ${connection.escape(data.pr_id)}
+  `
+  connection.query(sql, function (err, res) { err ? reject(new Error(err.message)) : resolve(res) })
+})
+
+Task.deletePurchaseRequestBy = (connection, data = {}) => new Promise((resolve, reject) => {
+  let sql = `DELETE FROM tb_purchase_request WHERE pr_id = ${connection.escape(data.pr_id)} `
+
+  connection.query(sql, function (err, res) { err ? reject(new Error(err.message)) : resolve(res) })
+})
+
+module.exports = Task
--- services/purchase-request.service.js
+++ services/purchase-request.service.js
@@ -2,14 +2,24 @@ const Task = function (task) { this.task = task.task }
 
 const { PurchaseRequestModel } = require('@/models')
 
-Task.generatePurchaseRequestID = (connection) => PurchaseRequestModel.generateStockInID(connection)
-Task.getPurchaseRequestBy = (connection, data) => UnitModel.getUnitBy(connection, data)
-Task.getPurchaseRequestByID = (connection, data) => UnitModel.getUnitByID(connection, data)
+Task.generatePurchaseRequestID = (connection) => PurchaseRequestModel.generatePurchaseRequestID(connection)
+Task.getPurchaseRequestBy = (connection, data) => PurchaseRequestModel.getPurchaseRequestBy(connection, data)
+Task.getPurchaseRequestByID = (connection, data) => PurchaseRequestModel.getPurchaseRequestByID(connection, data)
+Task.getPurchaseRequestPermissionBy = (connection, data) => PurchaseRequestModel.getPurchaseRequestPermissionBy(connection, data)
 
 Task.insertPurchaseRequest = async (connection, data) => {
-    data.pr_id = await PurchaseRequestModel.generatePurchaseRequestID(connection)
+    data.purchaserequest_id = await PurchaseRequestModel.generatePurchaseRequestID(connection)
     await PurchaseRequestModel.insertPurchaseRequest(connection, data);
-    return await PurchaseRequestModel.getPurchaseRequestByID(connection, { pr_id: data.pr_id });
+    return await PurchaseRequestModel.getPurchaseRequestByID(connection, { purchaserequest_id: data.purchaserequest_id });
 };
+
+Task.updatePurchaseRequestBy = async (connection, data) => {
+    await PurchaseRequestModel.updatePurchaseRequestBy(connection, data);
+    return await PurchaseRequestModel.getPurchaseRequestByID(connection, { purchaserequest_id: data.purchaserequest_id });
+}
+
+Task.deletePurchaseRequestBy = async (connection, data) => {
+    await PurchaseRequestModel.deletePurchaseRequestBy(connection, data)
+}
 
 module.exports = Task
